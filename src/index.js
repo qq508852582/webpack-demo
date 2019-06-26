416498487bbc7193d14eb13a97d57d07576d3a7c
JiangXiaoMing <>
2019-06-26 10:19:45 +0800

    no message

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import _ from 'lodash';
+import './style.css';
+import printMe from './print.js';
+
+function component() {
+    var element = document.createElement('div');
+
+    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需
+    // Lodash, now imported by this script
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+
+    // // 将图像添加到我们现有的 div。
+    // var myIcon = new Image();
+    // myIcon.src = Icon;
+    //
+    // element.appendChild(myIcon);
+
+    var btn = document.createElement('button');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
+
+    return element;
+}
+
+// document.body.appendChild(component());
+let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
+document.body.appendChild(element);
+if (module.hot) {
+    module.hot.accept('./print.js', function () {
+        console.log('Accepting the updated printMe module!');
+        // printMe();
+        document.body.removeChild(element);
+        element = component(); // 重新渲染页面后，component 更新 click 事件处理
+        document.body.appendChild(element);
+    })
+}
